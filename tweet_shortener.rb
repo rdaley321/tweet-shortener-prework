a8321069390ceb81ca70fed331264b0643f4808e
Ryan Daley <rdaley321@yahoo.com>
2018-11-07 11:28:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def dictionary 
+  hash = {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  tweet_array.each do |word|
+    if word == dictionary
+  end
+end
